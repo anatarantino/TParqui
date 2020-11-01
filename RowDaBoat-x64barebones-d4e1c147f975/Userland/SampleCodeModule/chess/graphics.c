00b5a6ea027027a0302b11abfe2e21c0ecfe66dc
martirodriguez98 <58373738+martirodriguez98@users.noreply.github.com>
2020-11-01 00:10:57 -0300

    SE IMPRIME EL TABLEROOO

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/chess/graphics.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/chess/graphics.c
@@ -6,8 +6,8 @@
 #define SCREENHEIGHT 768
 #define BOARDSIZE 760
 #define SQUARESIZE 87
-#define STARTX 160
-#define ENDX 860
+#define STARTX 157
+#define ENDX 847
 #define STARTY 4
 #define ENDY 700
 
@@ -15,13 +15,15 @@ void drawPixel(int x, int y, int color){
     syscalls(DRAW,0,0,color,0,x,y);
 }
 //(w>=STARTX && w<=STARTX+SQUARESIZE) && (h>=STARTY && h<=STARTY+SQUARESIZE) 
+
 void drawBoard(uint64_t color1, uint64_t color2){
     int paint=0;
     int noPaint=0;
     int cantX=0,cantY=0;
+
     uint64_t aux_color1=color1,aux_color2=color2;
     for(int h=0 ; h<SCREENHEIGHT ; h++){
-        if(cantY==87){
+        if((h-STARTY)%87 == 0){
             if(aux_color1==color1){
                 aux_color1=color2;
                 aux_color2=color1;
@@ -29,23 +31,62 @@ void drawBoard(uint64_t color1, uint64_t color2){
                 aux_color1=color1;
                 aux_color2=color2;
             }
-            cantY=0;
         }
         for(int w=0 ; w<SCREENWIDTH; w++){
             if((w>=STARTX && w<=ENDX) && (h>=STARTY && h<= ENDY)){
-                if((paint>=0 && paint<87)){
+                if(((w-STARTX)%87==0)){
+                    if(aux_color1==color1){
+                        aux_color1=color2;
+                        aux_color2=color1;
+                    }else{
+                        aux_color1=color1;
+                        aux_color2=color2;
+                    }
+                }
+                if(cantY>=0 && cantY<87){
                     drawPixel(w,h,aux_color2);
-                    paint++;
+                    cantY++;
                 }else{
                     drawPixel(w,h,aux_color1);
-                    noPaint++;
                 }
-                if(noPaint==86){
-                    paint=0;
-                    noPaint=0;
-                }
+
             }
         }
-        cantY++;
+        
     }
 }
+
+/*
+void drawBoard(uint64_t color1, uint64_t color2){
+    uint64_t aux_color1=color1,aux_color2=color2;
+    for(int y=STARTY ; y<ENDY ; y++){
+        if(y%87 == 0){
+            if(aux_color1==color1){
+                aux_color1=color2;
+                aux_color2=color1;
+            }else{
+                aux_color1=color1;
+                aux_color2=color2;
+            }
+        }
+        for(int x=STARTX ; x<ENDX ; x++){
+            if(x%87 == 0){
+                if(aux_color1==color1){
+                        aux_color1=color2;
+                        aux_color2=color1;
+                    }else{
+                        aux_color1=color1;
+                        aux_color2=color2;
+                    }
+            }
+
+            if(y>=0 && y<87){
+                drawPixel(x,y,aux_color2);
+            }else{
+                drawPixel(x,y,aux_color1);
+            }
+        }
+
+    }
+}
+*/
