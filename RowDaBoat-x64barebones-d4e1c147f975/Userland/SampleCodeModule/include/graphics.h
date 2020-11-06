a300700c280ef75e659d125c8a4e3c5fd607b8bb
Macarena Prado Torres <maca_pradot@hotmail.com>
2020-11-06 17:59:17 -0300

    piece()

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/include/graphics.h
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/include/graphics.h
@@ -8,6 +8,8 @@ void drawPixel(int x, int y, int color);
 void drawPiece(int piece, int x, int y, int pieceColor, int bgColor);
 void drawBoard(int matrix[][8],uint64_t color1, uint64_t color2,int rotation);
 
+void pieces(int ficha, int w, int h, int actualColor);
+
 
 
 #endif
